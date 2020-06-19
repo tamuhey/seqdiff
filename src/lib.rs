65c92cfeb5a6e0ee653216247b9e71675bc04391
Yohei Tamura <tamuhey@gmail.com>
2020-06-19 20:16:51 +0900

    add seqdiff lib

---
+++ src/lib.rs
@@ -0,0 +1,96 @@
+//! Diff between two Eq sequences
+#[cfg(test)]
+mod tests;
+use std::collections::HashMap;
+#[cfg(test)]
+extern crate quickcheck;
+#[cfg(test)]
+#[macro_use(quickcheck)]
+extern crate quickcheck_macros;
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+enum Node {
+    P((usize, usize)),
+    Root,
+}
+
+struct EditPath {
+    nodes_map: HashMap<Node, Node>,
+    cur: Node,
+}
+
+impl<'a> Iterator for EditPath {
+    type Item = (usize, usize);
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.cur {
+            Node::Root => None,
+            Node::P(cur) => {
+                self.cur = *self.nodes_map.get(&Node::P(cur)).unwrap();
+                Some(cur)
+            }
+        }
+    }
+}
+
+/// Returns an iterator over the shotest path of the edit graph based on Myers' diff algorithm.
+///
+/// See [An O(ND) Difference Algorithm and Its Variations](http://www.xmailserver.org/diff2.pdf)
+#[allow(clippy::many_single_char_names)]
+fn get_shortest_edit_path_myers<A: PartialEq<B>, B>(a: &Vec<A>, b: &Vec<B>) -> EditPath {
+    let n = a.len();
+    let m = b.len();
+    let bound = n + m;
+    let get_y = |x, k| x + bound - k;
+    let mut v = vec![0; 2 * bound + 1];
+    let mut nodes_map = HashMap::new();
+    'outer: for d in 0..=bound {
+        for k in ((bound - d)..=bound + d).step_by(2) {
+            let (mut x, parent) = if d == 0 {
+                (0, Node::Root)
+            } else if k == (bound - d) || k != (bound + d) && v[k - 1] < v[k + 1] {
+                let px = v[k + 1];
+                (px, Node::P((px, get_y(px, k + 1))))
+            } else {
+                let px = v[k - 1];
+                (px + 1, Node::P((px, get_y(px, k - 1))))
+            };
+            let mut y = get_y(x, k);
+            nodes_map.insert(Node::P((x, y)), parent);
+            while x < n && y < m && a[x] == b[y] {
+                nodes_map.insert(Node::P((x + 1, y + 1)), Node::P((x, y)));
+                x += 1;
+                y += 1;
+            }
+            v[k] = x;
+            if x >= n && y >= m {
+                break 'outer;
+            }
+        }
+    }
+
+    EditPath {
+        nodes_map,
+        cur: Node::P((n, m)),
+    }
+}
+
+pub type Diff = Vec<Option<usize>>;
+
+fn path_to_diff(mut path: impl Iterator<Item = (usize, usize)>) -> (Diff, Diff) {
+    let (mut i, mut j) = path.next().unwrap();
+    let mut a2b = vec![None; i];
+    let mut b2a = vec![None; j];
+    for (pi, pj) in path {
+        if (i - pi) + (j - pj) == 2 {
+            a2b[pi] = Some(pj);
+            b2a[pj] = Some(pi);
+        }
+        i = pi;
+        j = pj;
+    }
+    (a2b, b2a)
+}
+
+pub fn diff<A: PartialEq<B>, B>(a: &Vec<A>, b: &Vec<B>) -> (Diff, Diff) {
+    path_to_diff(get_shortest_edit_path_myers(a, b))
+}
---
+++ src/tests.rs
@@ -0,0 +1,81 @@
+use crate::*;
+#[cfg(test)]
+struct EditPathFromGrid {
+    // This struct is only for testing
+    // Inefficient but simple algorithm
+    d: Vec<Vec<usize>>,
+    cur: (usize, usize),
+    exhausted: bool,
+}
+
+#[cfg(test)]
+impl Iterator for EditPathFromGrid {
+    type Item = (usize, usize);
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.exhausted {
+            return None;
+        }
+        if self.cur == (0, 0) {
+            self.exhausted = true;
+            return Some((0, 0));
+        }
+        let (i, j) = self.cur;
+        let ncur = if i > 0 && j > 0 {
+            let ncur = *[(i, j - 1), (i - 1, j)]
+                .iter()
+                .min_by_key(|x| self.d[x.0][x.1])
+                .unwrap();
+            let ul = self.d[i - 1][j - 1];
+            if self.d[ncur.0][ncur.1] > ul && self.d[i][j] == ul {
+                (i - 1, j - 1)
+            } else {
+                ncur
+            }
+        } else if i > 0 {
+            (i - 1, j)
+        } else {
+            (i, j - 1)
+        };
+        self.cur = ncur;
+        Some((i, j))
+    }
+}
+
+#[allow(clippy::many_single_char_names)]
+#[cfg(test)]
+fn get_shortest_edit_path_grid<A: PartialEq<B>, B>(a: &Vec<A>, b: &Vec<B>) -> EditPathFromGrid {
+    let n = a.len();
+    let m = b.len();
+    let mut d = vec![vec![std::usize::MAX; m + 1]; n + 1];
+    d[0] = (0..=m).collect();
+    for (i, e) in d.iter_mut().enumerate().take(n + 1) {
+        e[0] = i;
+    }
+    for (i, ca) in a.iter().enumerate() {
+        for (j, cb) in b.iter().enumerate() {
+            if ca == cb {
+                d[i + 1][j + 1] = d[i][j];
+            } else {
+                let mut u = (i + 1, j);
+                let l = (i, j + 1);
+                if d[u.0][u.1] > d[l.0][l.1] {
+                    u = l;
+                }
+                d[i + 1][j + 1] = d[u.0][u.1] + 1;
+            }
+        }
+    }
+
+    EditPathFromGrid {
+        d,
+        cur: (n, m),
+        exhausted: false,
+    }
+}
+
+#[quickcheck]
+fn randomcheck_myers_with_dp(a: Vec<char>, b: Vec<char>) {
+    let v = path_to_diff(get_shortest_edit_path_grid(&a, &b));
+    let w = path_to_diff(get_shortest_edit_path_myers(&a, &b));
+    assert_eq!(v, w)
+}
